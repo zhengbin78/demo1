3ddcfdca79d5e47a46bb59d0eeadd834dccfc22b
zhengbin <zhengbin78@hotmail.com>
2019-10-28 22:51:47 +0800

    xxx

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,26 @@
+
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+  
+
+@RestController
+public class DemoController {
+
+	 
+    private final Logger log = LoggerFactory.getLogger(DemoController.class);
+
+	@GetMapping(value = "test1/{abc}")
+	public String test1(@PathVariable String abc) throws Exception {
+		log.info("参数值={}",abc);
+		return abc;
+	}
+	
+   
+}
