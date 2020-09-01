3e41eb2181e467509a56e81812e5702ac3209081
zhengbin <zhengbin78@hotmail.com>
2020-09-01 22:46:47 +0800

    up

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,29 +3,29 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-  
 
 @RestController
 public class DemoController {
 
-	 
-    private final Logger log = LoggerFactory.getLogger(DemoController.class);
+	@Value("${book.name}")
+	private String bookName; // 注入第一个配置外部文件属性
+
+	private final Logger log = LoggerFactory.getLogger(DemoController.class);
 
 	@GetMapping(value = "test1/{abc}")
 	public String test1(@PathVariable String abc) throws Exception {
-		log.info("参数值={}",abc);
+		log.info("参数值={}", abc);
 		return abc;
 	}
+
 	@GetMapping(value = "test1k")
 	public String test1k() throws Exception {
-		Thread.sleep(1000);  
+		Thread.sleep(1000);
 		return "test1k";
 	}
-	
-   
+
 }
